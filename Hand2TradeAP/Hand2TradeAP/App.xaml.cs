c8eb3596e9772e18c15c0a5ef949454264de0348
User <User@LAB3-16>
2021-10-20 14:57:25 +0300

    working on log in event

--- Hand2TradeAP/Hand2TradeAP/App.xaml.cs
+++ Hand2TradeAP/Hand2TradeAP/App.xaml.cs
@@ -2,15 +2,26 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Hand2TradeAP.Views;
+using Hand2TradeAP.Models;
 
 namespace Hand2TradeAP
 {
     public partial class App : Application
     {
+        public static bool IsDevEnv
+        {
+            get
+            {
+                return true; //change this before release!
+            }
+        }
+
+        //The current logged in user
+        public User CurrentUser { get; set; }
         public App()
         {
             InitializeComponent();
-
+            CurrentUser = null;
             MainPage = new SignUpPage();
         }
 
---
+++ Hand2TradeAP/Hand2TradeAP/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hand2TradeAP.Models
+{
+    public partial class User
+    {
+        public string Email { get; set; }
+        public string Passwrd { get; set; }
+        public string UserName { get; set; }
+        public bool IsAdmin { get; set; }
+        public int Coins { get; set; }
+        public int Reports { get; set; }       
+        public int TotalRank { get; set; }
+        public DateTime BearthDate { get; set; }
+        public string Adress { get; set; }
+        public bool IsBlocked { get; set; }
+    }
+}
